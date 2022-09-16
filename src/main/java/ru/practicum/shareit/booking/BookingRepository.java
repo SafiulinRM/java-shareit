f84fc73cd13ab9274a16f121d866163091cf1c01
safiulinrm <zloyangel666999@gmail.com>
2022-09-16 17:12:16 +0300

    fix "убрал _ из названий методов репозитория бронирований"

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -8,15 +8,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
-    List<Booking> findByBooker_IdOrderByStartDesc(long userId);
+    List<Booking> findByBookerIdOrderByStartDesc(long userId);
 
-    List<Booking> findByBooker_IdAndStartBeforeAndEndAfterOrderByStartDesc(long userId, LocalDateTime start, LocalDateTime end);
+    List<Booking> findByBookerIdAndStartBeforeAndEndAfterOrderByStartDesc(long userId, LocalDateTime start, LocalDateTime end);
 
-    List<Booking> findByBooker_IdAndEndBeforeOrderByStartDesc(long userId, LocalDateTime end);
+    List<Booking> findByBookerIdAndEndBeforeOrderByStartDesc(long userId, LocalDateTime end);
 
-    List<Booking> findByBooker_IdAndStartAfterOrderByStartDesc(long userId, LocalDateTime start);
+    List<Booking> findByBookerIdAndStartAfterOrderByStartDesc(long userId, LocalDateTime start);
 
-    List<Booking> findByBooker_IdAndStatusOrderByStartDesc(long userId, State status);
+    List<Booking> findByBookerIdAndStateOrderByStartDesc(long userId, State state);
 
     @Query("select b from Booking b " +
             "where b.item in (select i from Item i where i.ownerId = ?1) " +
@@ -39,13 +39,13 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> getBookingsOfOwnerInFuture(long userId, LocalDateTime now);
 
     @Query("select b from Booking b " +
-            "where b.item in (select i from Item i where i.ownerId = ?1) AND b.status = ?2 " +
+            "where b.item in (select i from Item i where i.ownerId = ?1) AND b.state = ?2 " +
             "order by b.start desc")
-    List<Booking> getBookingsOfOwnerAndStatus(long userId, State status);
+    List<Booking> getBookingsOfOwnerAndState(long userId, State state);
 
-    BookingShort findByItem_IdAndStartBefore(long itemId, LocalDateTime now);
+    BookingShort findByItemIdAndStartBefore(long itemId, LocalDateTime now);
 
-    BookingShort findByItem_IdAndStartAfter(long itemId, LocalDateTime now);
+    BookingShort findByItemIdAndStartAfter(long itemId, LocalDateTime now);
 
-    List<Booking> findByBooker_Id(long bookerId);
+    List<Booking> findByBookerId(long bookerId);
 }
