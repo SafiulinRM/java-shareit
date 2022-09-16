974630358ea4cd65ec9ce123b85c1621788708d6
safiulinrm <zloyangel666999@gmail.com>
2022-09-16 17:05:53 +0300

    fix "вывел константу DEFAULT_STATE"

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -2,48 +2,54 @@ package ru.practicum.shareit.booking;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.shareit.booking.dto.BookingDto;
-import ru.practicum.shareit.booking.dto.BookingDtoInfo;
+import ru.practicum.shareit.booking.dto.BookingDtoInput;
+import ru.practicum.shareit.booking.dto.BookingDtoOutput;
 
 import java.util.Collection;
 
 import static java.util.stream.Collectors.toList;
-import static ru.practicum.shareit.booking.BookingMapper.toBookingDtoInfo;
+import static ru.practicum.shareit.booking.BookingMapper.toBookingDtoOutput;
 
 @RestController
 @RequestMapping(path = "/bookings")
 @RequiredArgsConstructor
 public class BookingController {
-    public static final String HEADER_OF_OWNER = "X-Sharer-User-Id";
+    public static final String USER_ID_HEADER = "X-Sharer-User-Id";
+    public static final String DEFAULT_STATE = "ALL";
     private final BookingService bookingService;
 
     @PostMapping
-    public BookingDtoInfo add(@RequestHeader(HEADER_OF_OWNER) long userId, @RequestBody BookingDto bookingDto) {
-        return toBookingDtoInfo(bookingService.add(userId, bookingDto));
+    public BookingDtoOutput add(@RequestHeader(USER_ID_HEADER) long userId, @RequestBody BookingDtoInput bookingDto) {
+        return toBookingDtoOutput(bookingService.add(userId, bookingDto));
     }
 
     @PatchMapping("/{bookingId}")
-    public BookingDtoInfo approveBooking(@PathVariable long bookingId,
-                                         @RequestParam Boolean approved, @RequestHeader(HEADER_OF_OWNER) long userId) {
-        return toBookingDtoInfo(bookingService.approveBooking(bookingId, approved, userId));
+    public BookingDtoOutput approveBooking(@PathVariable long bookingId,
+                                           @RequestParam Boolean approved,
+                                           @RequestHeader(USER_ID_HEADER) long userId) {
+        return toBookingDtoOutput(bookingService.approveBooking(bookingId, approved, userId));
     }
 
     @GetMapping("/{bookingId}")
-    public BookingDtoInfo getById(@PathVariable long bookingId, @RequestHeader(HEADER_OF_OWNER) long userId) {
-        return toBookingDtoInfo(bookingService.getById(bookingId, userId));
+    public BookingDtoOutput getById(@PathVariable long bookingId, @RequestHeader(USER_ID_HEADER) long userId) {
+        return toBookingDtoOutput(bookingService.getById(bookingId, userId));
     }
 
     @GetMapping()
-    public Collection<BookingDtoInfo> getBookingsOfUser(@RequestHeader(HEADER_OF_OWNER) long userId,
-                                                        @RequestParam(required = false, defaultValue = "ALL") String state) {
-        return bookingService.getBookingsOfUser(userId, state)
-                .stream().map(BookingMapper::toBookingDtoInfo).collect(toList());
+    public Collection<BookingDtoOutput> getBookingsOfUser
+            (@RequestHeader(USER_ID_HEADER) long userId,
+             @RequestParam(required = false, defaultValue = DEFAULT_STATE) String state) {
+        return bookingService.getBookingsOfUser(userId, state).stream()
+                .map(BookingMapper::toBookingDtoOutput)
+                .collect(toList());
     }
 
     @GetMapping("/owner")
-    public Collection<BookingDtoInfo> getBookingsForAllItemsOfUser(@RequestHeader(HEADER_OF_OWNER) long userId,
-                                                                   @RequestParam(required = false, defaultValue = "ALL") String state) {
-        return bookingService.getBookingsForAllItemsOfUser(userId, state)
-                .stream().map(BookingMapper::toBookingDtoInfo).collect(toList());
+    public Collection<BookingDtoOutput> getBookingsForAllItemsOfUser
+            (@RequestHeader(USER_ID_HEADER) long userId,
+             @RequestParam(required = false, defaultValue = DEFAULT_STATE) String state) {
+        return bookingService.getBookingsForAllItemsOfUser(userId, state).stream()
+                .map(BookingMapper::toBookingDtoOutput)
+                .collect(toList());
     }
 }
