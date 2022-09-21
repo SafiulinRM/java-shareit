b7b9cab1705b0843447192db123af6cb0a4c51cf
safiulinrm <zloyangel666999@gmail.com>
2022-09-21 05:56:03 +0300

    fix "добавил обработчики и логирование"

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -1,42 +1,63 @@
 package ru.practicum.shareit.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-
+@Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationException(final ValidationException e) {
+        log.warn("Ошибка валидации." + e.getMessage());
         return new ErrorResponse("Ошибка валидации.", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(final NotFoundException e) {
+        log.warn("Искомый объект не найден." + e.getMessage());
         return new ErrorResponse("Искомый объект не найден.", e.getMessage());
     }
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleConstraintViolationException(final ConstraintViolationException e) {
+        log.warn("Конфликт с базой данных" + e.getMessage());
         return new ErrorResponse("Конфликт с базой данных", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleUnsupportedStateException(final UnsupportedStateException e) {
+        log.warn("Unknown state: UNSUPPORTED_STATUS" + e.getMessage());
         return new ErrorResponse("Unknown state: UNSUPPORTED_STATUS", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleBookingException(final BookingException e) {
+        log.warn("Ошибка с бронированием" + e.getMessage());
         return new ErrorResponse("Ошибка с бронированием", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleThrowable(final Throwable e) {
+        log.warn("Неизвестная ошибка" + e.getMessage());
+        return new ErrorResponse("Неизвестная ошибка", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        log.warn("Неправильные поля у объекта" + e.getMessage());
+        return new ErrorResponse("Неправильные поля у объекта", e.getMessage());
     }
 }
