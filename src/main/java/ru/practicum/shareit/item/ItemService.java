c52bfd8bfdeafbee670a55bfda8aef8afc31ed10
safiulinrm <zloyangel666999@gmail.com>
2022-09-29 22:06:01 +0300

    fix "перенес маппирование в сервис"

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -2,21 +2,20 @@ package ru.practicum.shareit.item;
 
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.model.Comment;
-import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.dto.ItemDtoEnlarged;
 
 import java.util.Collection;
 
 public interface ItemService {
-    Item add(ItemDto itemDto, long userId);
+    ItemDto add(ItemDto itemDto, long userId);
 
-    Item update(ItemDto itemDto, long userId, long itemId);
+    ItemDto update(ItemDto itemDto, long userId, long itemId);
 
-    Item getByItemId(long itemId, long userId);
+    ItemDtoEnlarged getByItemId(long itemId, long userId);
 
-    Collection<Item> getItemsOfOwner(long userId);
+    Collection<ItemDtoEnlarged> getItemsOfOwner(long userId, int from, int size);
 
-    Collection<Item> getRequestedItems(String text);
+    Collection<ItemDto> getRequestedItems(String text, int from, int size);
 
-    Comment addComment(long itemId, long authorId, CommentDto commentDto);
+    CommentDto addComment(long itemId, long authorId, CommentDto commentDto);
 }
--- src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
@@ -2,8 +2,10 @@ package ru.practicum.shareit.item;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.booking.Booking;
 import ru.practicum.shareit.booking.BookingRepository;
@@ -13,6 +15,7 @@ import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.exception.ValidationException;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ItemDtoEnlarged;
 import ru.practicum.shareit.item.model.Comment;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.User;
@@ -23,35 +26,34 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import static java.util.stream.Collectors.toList;
-import static ru.practicum.shareit.item.CommentMapper.toComment;
-import static ru.practicum.shareit.item.CommentMapper.toCommentsDto;
-import static ru.practicum.shareit.item.ItemMapper.toItem;
-import static ru.practicum.shareit.item.ItemMapper.updateItem;
+import static ru.practicum.shareit.item.CommentMapper.*;
+import static ru.practicum.shareit.item.ItemMapper.*;
 
 @Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ItemServiceImpl implements ItemService {
+    private static final Sort DEFAULT_SORT = Sort.by(Sort.Direction.ASC, "id");
     private final ItemRepository itemRepository;
     private final UserRepository userRepository;
     private final BookingRepository bookingRepository;
     private final CommentRepository commentRepository;
 
     @Override
-    @Transactional(isolation = Isolation.SERIALIZABLE)
-    public Item add(ItemDto itemDto, long userId) {
+    @Transactional
+    public ItemDto add(ItemDto itemDto, long userId) {
         User owner = userRepository.findById(userId)
                 .orElseThrow(() -> new NotFoundException("User not found " + userId));
         Item item = toItem(itemDto, owner);
         Item newItem = itemRepository.save(item);
         log.info("Added item name: {}", item.getName());
-        return newItem;
+        return toItemDto(newItem);
     }
 
     @Override
     @Transactional
-    public Item update(ItemDto itemDto, long userId, long itemId) {
+    public ItemDto update(ItemDto itemDto, long userId, long itemId) {
         Item updateItem = updateItem(itemDto, itemId);
         Item newItem = itemRepository.findById(itemId)
                 .orElseThrow(() -> new NotFoundException("Item not found " + itemId));
@@ -69,48 +71,51 @@ public class ItemServiceImpl implements ItemService {
             newItem.setAvailable(updateItem.getAvailable());
         }
         log.info("Updated item id: {}", itemId);
-        return newItem;
+        return toItemDto(newItem);
     }
 
     @Override
-    public Item getByItemId(long itemId, long userId) {
+    public ItemDtoEnlarged getByItemId(long itemId, long userId) {
         Item newItem = itemRepository.findById(itemId).orElseThrow(() -> new NotFoundException("item not found " + itemId));
         setBookingsOfItem(newItem, userId);
         Collection<CommentDto> comments = toCommentsDto(commentRepository.findByItemId(itemId));
         newItem.setComments(comments);
         log.info("Got user id: {}", itemId);
-        return newItem;
+        return toItemDtoEnlarged(newItem);
     }
 
     @Override
-    public Collection<Item> getItemsOfOwner(long userId) {
-        Collection<Item> itemsDto = itemRepository.findByOwnerIdOrderByIdAsc(userId);
-        log.info("Got items: {}", itemsDto.size());
-        return itemsDto.stream()
+    public Collection<ItemDtoEnlarged> getItemsOfOwner(long userId, int from, int size) {
+        int page = from / size;
+        Page<Item> itemsDto = itemRepository.findByOwnerId(userId, PageRequest.of(page, size, DEFAULT_SORT));
+        log.info("Got items");
+        return toItemsDtoEnlarged(itemsDto.stream()
                 .map(it -> setBookingsOfItem(it, userId))
-                .collect(toList());
+                .collect(toList()));
     }
 
     @Override
-    public Collection<Item> getRequestedItems(String text) {
+    public Collection<ItemDto> getRequestedItems(String text, int from, int size) {
         if (text.isBlank()) {
             return new ArrayList<>();
         }
-        Collection<Item> items = itemRepository
-                .search(text);
-        log.info("Got items: {}", items.size());
-        return items;
+        int page = from / size;
+        Page<Item> items = itemRepository
+                .search(text, PageRequest.of(page, size, DEFAULT_SORT));
+        log.info("Got items");
+        return toItemsDto(items.stream()
+                .collect(toList()));
     }
 
     @Override
     @Transactional
-    public Comment addComment(long itemId, long authorId, CommentDto commentDto) {
+    public CommentDto addComment(long itemId, long authorId, CommentDto commentDto) {
         Item item = itemRepository.findById(itemId).orElseThrow(() -> new NotFoundException("item not found " + itemId));
         User author = userRepository.findById(authorId)
                 .orElseThrow(() -> new NotFoundException("User not found " + item.getOwner().getId()));
         validationComment(authorId, itemId);
         Comment newComment = toComment(commentDto, author, item);
-        return commentRepository.save(newComment);
+        return toCommentDto(commentRepository.save(newComment));
     }
 
     private void validationComment(long authorId, long itemId) {
